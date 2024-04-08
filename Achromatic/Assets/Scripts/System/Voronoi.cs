8ee0086f61373d15d1ec4bf338249671d86b57ba
D-buger <o3oduck@naver.com>
2024-04-08 22:40:25 +0900

    [#8] 스프라이트 뽀개기용 보로노이 테스트

---
+++ Achromatic/Assets/Scripts/System/Voronoi.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voronoi : MonoBehaviour
+{
+    public Vector2Int imageDim;
+    public int regionAmount;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = Sprite.Create(GetDiagram(), new Rect(0, 0, imageDim.x, imageDim.y), Vector2.one * 0.5f);
+    }
+
+    Texture2D GetDiagram()
+    {
+        Vector2Int[] centroids = new Vector2Int[regionAmount];
+        Color[] regions = new Color[regionAmount];
+        for(int i = 0; i < regionAmount; i++)
+        {
+            centroids[i] = new Vector2Int(Random.Range(0, imageDim.x), Random.Range(0, imageDim.y));
+            regions[i] = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+        }
+        Color[] pixelColors = new Color[imageDim.x * imageDim.y];
+        for(int x = 0; x < imageDim.x; x++)
+        {
+            for(int y = 0; y < imageDim.y; y++)
+            {
+                int index = x * imageDim.x + y;
+                pixelColors[index] = regions[GetClosestCentroidIndex(new Vector2Int(x, y), centroids)];
+            }
+        }
+        return GetImageFromColorArray(pixelColors);
+    }
+
+    int GetClosestCentroidIndex(Vector2Int pixelpos, Vector2Int[] centroids)
+    {
+        float smallestDst = float.MaxValue;
+        int index = 0;
+        for(int i = 0; i < centroids.Length; i++)
+        {
+            if(Vector2.Distance(pixelpos, centroids[i]) < smallestDst)
+            {
+                smallestDst = Vector2.Distance(pixelpos, centroids[i]);
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    Texture2D GetImageFromColorArray(Color[] pixelColors)
+    {
+        Texture2D tex = new Texture2D(imageDim.x, imageDim.y);
+        tex.filterMode = FilterMode.Point;
+        tex.SetPixels(pixelColors);
+        tex.Apply();
+        return tex;
+    }
+}
