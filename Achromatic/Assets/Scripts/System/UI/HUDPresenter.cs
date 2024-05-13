10c61f2d6b3ab3ddd13afc46e56aa71c89334382
mu8991 <kafkablue72@gmail.com>
2024-05-13 21:24:23 +0900

    [#23] 체리픽 - [#1] 메인 ui 0513 추가사항 [a90e7d2]

--- Achromatic/Assets/Scripts/System/UI/HUDPresenter.cs
+++ Achromatic/Assets/Scripts/System/UI/HUDPresenter.cs
@@ -3,14 +3,20 @@ using UnityEngine.UI;
 public class HUDPresenter : MonoBehaviour
 {
     private UIElement<HealthBar> HealthBar;
-    private UIElement<Slider> BossHpSlider;
+    private UIElement<Image> BossHpSlider;
 
     private void Awake()
     {
         HealthBar = new UIElement<HealthBar>("HealthBar", this.gameObject);
-        //BossHpSlider = new UIElement<Slider>("BossHP", this.gameObject);
+        BossHpSlider = new UIElement<Image>("BossHP", this.gameObject);
 
         PlayManager.Instance.GetPlayer.PlayerCurrentHPEvent += HealthBar.Component.SetInsideGraphic;
         PlayManager.Instance.GetPlayer.PlayerMaxHPEvent += HealthBar.Component.SizeChange;
+
+
+    }
+    public void BossHpSliderSetValue(int maxValue, int value)
+    {
+        BossHpSlider.Component.fillAmount = Mathf.Clamp(value, 0, 100);
     }
 }
